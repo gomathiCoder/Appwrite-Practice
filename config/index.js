36467fe318b99b1823075d5059371349f7ad7ad8
gomathiCoder <91526592+gomathiCoder@users.noreply.github.com>
2022-10-27 23:43:42 +0530

    feat: Create config file for appwrite Authentication

---
+++ config/index.js
@@ -0,0 +1,10 @@
+import { Client, Appwrite, Account} from 'appwrite';
+
+const client = new Client();
+const account = new Account(client);
+
+client
+.setEndpoint('http://192.168.43.170/v1')
+.setProject('63599f5a842a3fd925ec');
+
+export default account;
