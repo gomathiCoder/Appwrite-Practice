5f8aa3ffe1a6f283185a83fd4c929191f3620dd4
gomathiCoder <91526592+gomathiCoder@users.noreply.github.com>
2022-10-28 22:53:17 +0530

    feat: Add Sign out functionality

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -1,10 +1,26 @@
 import React from "react";
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Button} from 'react-native';
+import account from "../config/index";
 
-export default function Dashboard(){
+export default function Dashboard({navigation}){
+    
+    function handleSignOutSubmit(){
+        account
+        .deleteSession('current')
+        .then(response => {
+            if(!response.message){
+                navigation.replace("SignIn");
+            }
+        },
+        error => console.log(error))
+    }
+
     return (
         <View style={styles.container}>
             <Text>Welcome to Dashboard</Text>
+            <View style={{margin:10}}>
+        <Button title="Sign Out" onPress={() => handleSignOutSubmit()} />
+      </View>
         </View>
     )
 }
